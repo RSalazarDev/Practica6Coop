e6e371aa23a1335e58c7adadf0248b0ba71c9965
Javier <Javier@DESKTOP-J3213LP.Home>
2019-06-01 20:12:42 +0200

    cliente version 1.0

--- src/practica6/Cliente.java
+++ src/practica6/Cliente.java
@@ -10,5 +10,22 @@ package practica6;
  * @author dam03
  */
 public class Cliente {
-    
+    String nombre,apellidos,direccion,telefono;
+    int edad,codigo;
+
+    public Cliente(String nombre, String apellidos, String direccion, String telefono, int edad, int codigo) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.edad = edad;
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", direccion=" + direccion + ", telefono=" + telefono + ", edad=" + edad + ", codigo=" + codigo + '}';
+    }
+
+  
 }
